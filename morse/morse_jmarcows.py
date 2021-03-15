724b67ee6435eb9cdc575baf05a2e6ab367a76f1
Jakub Mateusz Marcowski <01158831@pw.edu.pl>
2021-03-15 20:35:04 +0100

    make .gitignore, first working version of morse_jmarcows.py

--- morse/morse_jmarcows.py
+++ morse/morse_jmarcows.py
@@ -1,3 +1,6 @@
+import argparse
+import sys
+
 letter_to_morse = {
     "a": ".-",
     "b": "-...",
@@ -28,18 +31,32 @@ letter_to_morse = {
 }
 
 
-def file_to_array(file):
-    f = open(file, "r")
-    array_of_lines = []
-    for line in f:
-        array_of_lines.append(line)
-    f.close()
-    return array_of_lines
+def main(arguments):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('file', type=argparse.FileType('r'), nargs='+')
+
+    args = parser.parse_args(arguments[1:])
+    morse = []
+
+    for f in args.file:
+        for line in f:
+            l = line.rstrip()
+            morse_line = ""
+            for letter in l:
+                if letter == " ":
+                    morse_line += " /"
+                elif letter.lower() in letter_to_morse.keys():
+                    morse_line += f" {letter_to_morse[letter.lower()]}"
+            morse.append(morse_line)
+    
+    for i in range(len(morse)):
+        while " / /" in morse[i]:
+            morse[i] = morse[i].replace(" / /", " /")
+    
+    for line in morse:
+        print(line)
 
 
-# quick test
-y = letter_to_morse["y"]
-print(y)
-
-# can you hear me GitLab?
-# I mean it this time
+if __name__ == "__main__":
+    main(sys.argv)
