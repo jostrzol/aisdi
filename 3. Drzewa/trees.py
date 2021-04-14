3ac1c8e60d1490c39f678f8014657635b2f21ee8
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2021-04-14 21:47:19 +0200

    split to BST_node and BST, add AVL with insert

--- 3. Drzewa/trees.py
+++ 3. Drzewa/trees.py
@@ -1,10 +1,17 @@
-class BST:
+from __future__ import annotations
+from typing import Optional
+from random import sample, seed
 
-    def __init__(self, data):
 
-        self.left = None
-        self.right = None
+class BST_node:
+
+    def __init__(self, data, parent: Optional[BST_node] = None):
+
+        self.left: Optional[BST_node] = None
+        self.right: Optional[BST_node] = None
         self.data = data
+
+        self.parent = parent
 
 # Insert Node
     def insert(self, data):
@@ -12,12 +19,12 @@ class BST:
         if self.data:
             if data < self.data:
                 if self.left is None:
-                    self.left = BST(data)
+                    self.left = BST_node(data, parent=self.left)
                 else:
                     self.left.insert(data)
             elif data > self.data:
                 if self.right is None:
-                    self.right = BST(data)
+                    self.right = BST_node(data, parent=self.right)
                 else:
                     self.right.insert(data)
         else:
@@ -32,21 +39,182 @@ class BST:
             self.right.PrintTree()
 
 # Left -> Root -> Right
-    def InOrderTraversal(self, root):
+    def InOrderTraversal(self) -> list:
         res = []
-        if root:
-            res = self.InOrderTraversal(root.left)
-            res.append(root.data)
-            res = res + self.InOrderTraversal(root.right)
+        if self.left:
+            res = self.left.InOrderTraversal()
+        res.append(self.data)
+        if self.right:
+            res = res + self.right.InOrderTraversal()
         return res
 
 
+class BST():
+    def __init__(self, data=None):
+        self._root = BST_node(data)
+
+    def insert(self, data):
+        self._root.insert(data)
+        if self._root.parent is not None:
+            self._root = self._root.parent
+
+    def InOrderTraversal(self) -> list:
+        return self._root.InOrderTraversal()
+
+
+class AVL_node(BST_node):
+
+    def __init__(self, data, parent: Optional[AVL_node] = None):
+        super().__init__(data, parent=parent)
+        self.parent: AVL_node
+        self.left: AVL_node
+        self.right: AVL_node
+        self._balance = 0
+
+    def _propagate_balance(self):
+        parent = self.parent
+        if parent is None:
+            return
+
+        grown_child = None
+        if self._balance == -1:
+            grown_child = self.left
+        elif self._balance == 1:
+            grown_child = self.right
+
+            if self is parent.left:
+                parent._balance -= 1
+            else:
+                parent._balance += 1
+
+            if abs(parent._balance) > 1:
+                if parent._balance * self._balance < 0:
+                    # unbalanced tree is between self and parent, need two roations
+                    grown_child._rotate()
+                    grown_child._rotate()
+
+                    growth_point = parent._balance * grown_child._balance
+
+                    if growth_point == 0:
+                        # grown_child._balance must have been 0, so now everything is balanced
+                        parent._balance = 0
+                        self._balance = 0
+                    elif growth_point > 0:
+                        # grown tree is now in the "inner side" of self,
+                        # so parent must be unbalanced to the "inner side" and self is balanced;
+                        # inner side is left if self._balance == -1 and right if self._balance == 1
+
+                        parent._balance = self._balance
+                        self._balance = 0
+                    else:
+                        # grown tree is now in the "inner side" of parent,
+                        # so self must be unbalanced to the "inner side" and parent is balanced;
+                        # inner side is left if parent._balance/2 == -1 and right if parent._balance/2 == 1
+
+                        self._balance = parent._balance/2
+                        parent._balance = 0
+
+                    # either way grown_child is on top and is balanced
+                    grown_child._balance = 0
+
+                    parent = grown_child  # grown_child comes on top
+                else:
+                    # self is between the unbalanced tree and parent, need one roation
+                    self._rotate()
+                    parent._balance = 0
+                    self._balance = 0
+                    parent = self  # self comes on top
+
+            if parent is not None and parent._balance != 0:
+                parent._propagate_balance()
+
+    def _rotate(self):
+        if self.parent is None:
+            raise NoParentError(self)
+        parent = self.parent
+        grandpa = parent.parent
+        if grandpa is not None:
+            if parent is grandpa.left:
+                grandpa.left = self
+            else:
+                grandpa.right = self
+        self.parent = grandpa
+        parent.parent = self
+
+        if self is parent.right:
+            parent.right = self.left
+            if self.left is not None:
+                self.left.parent = parent
+            self.left = parent
+        else:
+            parent.left = self.right
+            if self.right is not None:
+                self.right.parent = parent
+            self.right = parent
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = AVL_node(data)
+                    self.left.parent = self
+                    self.left._propagate_balance()
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = AVL_node(data)
+                    self.right.parent = self
+                    self.right._propagate_balance()
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+
+class AVL(BST):
+    def __init__(self, data=None):
+        self._root = AVL_node(data)
+
+
+class NoParentError(Exception):
+    def __init__(self, node: AVL_node):
+        super().__init__('The given node does not have a parent')
+        self.node: AVL_node = node
+
+
 # quick test
-root = BST(27)
-root.insert(14)
-root.insert(35)
-root.insert(10)
-root.insert(19)
-root.insert(31)
-root.insert(42)
-print(root.InOrderTraversal(root))
+if __name__ == "__main__":
+    seed('trees')
+
+    # a1 = AVL(27)
+    # a1.insert(14)
+    # a1.insert(35)
+    # a1.insert(16)
+    # a1.insert(15)
+    # a1.insert(31)
+    # a1.insert(42)
+    # print(a1.InOrderTraversal())
+
+    a = AVL()
+    for i in sample(range(3000), 1000):
+        a.insert(i)
+    print(a.InOrderTraversal())
+
+    a2 = AVL(27)
+    a2.insert(14)
+    a2.insert(35)
+    a2.insert(10)
+    a2.insert(9)
+    a2.insert(31)
+    a2.insert(42)
+    print(a2.InOrderTraversal())
+
+    b1 = BST(27)
+    b1.insert(14)
+    b1.insert(35)
+    b1.insert(10)
+    b1.insert(19)
+    b1.insert(31)
+    b1.insert(42)
+    print(b1.InOrderTraversal())
