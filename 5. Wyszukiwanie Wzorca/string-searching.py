e5c057d45b8fa5b14c4ce9f64179973f6760bc35
Jakub Mateusz Marcowski <01158831@pw.edu.pl>
2021-05-17 15:06:58 +0200

    fix find_n() to do what it's supposed to do and add find_kmp

--- 5. Wyszukiwanie Wzorca/string-searching.py
+++ 5. Wyszukiwanie Wzorca/string-searching.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class StringSearching:
-    def __init__(self, txt: str, pat: str):
+    def __init__(self, txt: str, pat: str) -> None:
         """
         Parameters:
             txt: text to be searched
@@ -19,14 +19,16 @@ class StringSearching:
         """
         Naive Search
         """
+        match_indices = []
+
         M = len(self._pattern)
         N = len(self._text)
 
-        # A loop to slide pat[] one by one
+        # a loop to slide pat[] one by one
         for i in range(N - M + 1):
             j = 0
 
-            # For current index i, check
+            # for current index i, check
             # for pattern match
             while(j < M):
                 if (self._text[i + j] != self._pattern[j]):
@@ -36,20 +38,68 @@ class StringSearching:
             if (j == M):
                 # return the pattern's
                 # indexes in text
-                return list(range(i, (i + M)))
+                match_indices.append(i)
+
+        return match_indices
 
     def find_kmp(self) -> List[int]:
         """
         Knuth–Morris–Pratt algorithm
         """
-        pass
+        match_indices = []
+        pattern_lps = self.compute_lps()
 
-    def find_kr(self) -> List[int]:
+        pattern_i = 0
+        for i, ch in enumerate(self._text):
+
+            # if a mismatch was found, roll back the pattern
+            # index using the information in LPS
+            while pattern_i and self._pattern[pattern_i] != ch:
+                pattern_i = pattern_lps[pattern_i - 1]
+
+            # if match
+            if self._pattern[pattern_i] == ch:
+                # if the end of a pattern is reached, record a result
+                # and use infromation in LSP array to shift the index
+                if pattern_i == len(self._pattern) - 1:
+                    match_indices.append(i - pattern_i)
+                    pattern_i = pattern_lps[pattern_i]
+
+                else:
+                    # move the pattern index forward
+                    pattern_i += 1
+
+        return match_indices
+
+    def find_rk(self) -> List[int]:
         """
         Rabin–Karp algorithm
         """
         pass
 
+    def compute_lps(self) -> List[int]:
+        # Longest Proper Prefix that is suffix array (LPS)
+        lps = [0] * len(self._pattern)
 
-string1 = StringSearching("abcABC123qwerty", "qwe")
-print(string1.find_n())  # should print out "[9, 10, 11]"
+        prefi = 0
+        for i in range(1, len(self._pattern)):
+
+            # roll the prefix pointer back until match
+            # or beginning of pattern is reached
+            while prefi and self._pattern[i] != self._pattern[prefi]:
+                prefi = lps[prefi - 1]
+
+            # if match, record the LSP for the
+            # current 'i' and move prefix pointer
+            if self._pattern[prefi] == self._pattern[i]:
+                prefi += 1
+                lps[i] = prefi
+
+        return lps
+
+
+string1 = StringSearching("abcABC123qwertyqwer", "qwe")
+
+# all should print out "[9, 15]"
+print(string1.find_n())
+print(string1.find_kmp())
